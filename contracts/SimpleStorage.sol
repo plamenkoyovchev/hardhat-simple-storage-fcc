0e03cff17e49691a37897e9375083f08c8a27d1b
plamenkoyochev <plamenkooo@gmail.com>
2022-11-27 10:09:57 +0200

    Initialize owner favorite number.

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -8,6 +8,10 @@ contract SimpleStorage {
     uint256 s_lastStoredFavoriteNumber;
     mapping(address => uint) s_addressToFavoriteNumber;
 
+    constructor() {
+        s_addressToFavoriteNumber[msg.sender] = 77;
+    }
+
     function store(uint number) external {
         console.log("Storing number %s", number);
 
